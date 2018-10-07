568b1ea695646dd502eca1451647407c79d13a03
AkhilaGarlapally <g.akhila102@gmail.com>
2018-10-07 10:02:37 -0700

    Update jobdsl.groovy

--- test/jobdsl.groovy
+++ test/jobdsl.groovy
@@ -2,11 +2,11 @@ node('master'){
     stage('checkout')
             {
 
-                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '9b5320b1-2e43-4fc0-b906-9a6f4cfec42f', url: 'https://github.com/AkhilaGarlapally/dev.git']]]
+                checkout scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github', url: 'https://github.com/AkhilaGarlapally/dev.git']]]
             }
     stage('jobdsl')
             {
-                jobDsl target : jobdslscripts
+                jobDsl targets : jobdslscripts
             }
 
 }
